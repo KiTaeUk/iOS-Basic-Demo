05ec5cddfdcebe680adb870a4526e8d4fa301c64
KiTaeUk <ktw1421@gmail.com>
2023-03-03 16:56:33 +0900

    [CH4] multi세그웨이

---
+++ CH4/Scene-MultiUnwind/Scene-MultiUnwind/Page2ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  Page2ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by 기태욱 on 2023/03/03.
+//
+
+import UIKit
+
+class Page2ViewController : UIViewController {
+    
+    @IBAction func gotoPage2(_ segue : UIStoryboardSegue){
+        
+        
+    }
+}
---
+++ CH4/Scene-MultiUnwind/Scene-MultiUnwind/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Scene-MultiUnwind
+//
+//  Created by 기태욱 on 2023/03/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func gotoPage1(_ sender : UIStoryboardSegue){
+        
+    }
+}
+
