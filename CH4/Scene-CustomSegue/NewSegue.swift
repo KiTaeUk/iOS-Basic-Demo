14a6492537fed8bd400b375bf96708530b6ad401
KiTaeUk <ktw1421@gmail.com>
2023-03-03 17:27:24 +0900

    [CH4] 커스텀 세그웨이

---
+++ CH4/Scene-CustomSegue/NewSegue.swift
@@ -0,0 +1,25 @@
+//
+//  NewSegue.swift
+//  Scene-CustomSegue
+//
+//  Created by 기태욱 on 2023/03/03.
+//
+
+import UIKit
+
+class NewSegue : UIStoryboardSegue {
+    
+    // UIStoryboardSegue 클래스에서 세그웨이의 실행을 처리하는 메서드가 perform()
+    // 커스텀 세그웨이에서도 원하는 화면 전환 기능을 구현하기 위해서는 perform() 메서드를 override 해야함
+    
+    override func perform(){
+        // 세그웨이 출발 VC
+        let srcUVC = self.source
+        
+        // 세그웨이 도착 VC
+        let destUVC = self.destination
+        
+        // formView에서 toView로 뷰를 전환한다
+        UIView.transition(from: srcUVC.view, to: destUVC.view, duration: 2, options: .transitionCurlDown)
+    }
+}
---
+++ CH4/Scene-CustomSegue/Scene-CustomSegue/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Scene-CustomSegue
+//
+//  Created by 기태욱 on 2023/03/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if (segue.identifier == "custom_segue"){
+            NSLog("커스텀 세그가 실행됩니다")
+        } else if(segue.identifier == "action_segue"){
+            NSLog("액션 세그가 실행됩니다")
+
+        } 
+        
+        
+        
+    }
+}
+
