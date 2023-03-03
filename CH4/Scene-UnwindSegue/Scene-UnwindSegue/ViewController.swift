8a6407e861f688576b0bcaaafa4318c0646a7fde
KiTaeUk <ktw1421@gmail.com>
2023-03-03 16:46:37 +0900

    [CH4] unwind Segue

---
+++ CH4/Scene-UnwindSegue/Scene-UnwindSegue/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Scene-UnwindSegue
+//
+//  Created by 기태욱 on 2023/03/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindToVC(_ segue: UIStoryboardSegue){
+        
+    }
+
+}
+
