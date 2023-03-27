05e0bcb427a8a0f68d3abc88157b38f87fb1e5c5
KiTaeUk <ktw1421@gmail.com>
2023-03-27 14:43:54 +0900

    [CH5] 다양한 방법의 값 전달

---
+++ CH5/SubmitValue-Back/SubmitValue-Back/FormViewController.swift
@@ -0,0 +1,63 @@
+//
+//  FormViewController.swift
+//  SubmitValue-Back
+//
+//  Created by 기태욱 on 2023/03/27.
+//
+
+import Foundation
+import UIKit
+
+
+class FormViewController : UIViewController {
+    
+    @IBOutlet var email : UITextField!
+    @IBOutlet var isUpdate : UISwitch!
+    @IBOutlet var interval : UIStepper!
+    
+    @IBAction func onSubmit(_ sender : Any){
+        
+//        // presentingViewController 속성을 통해 이전화면 객체를 읽어온 다음,
+//        // ViewController 타입으로 캐스팅한다
+//        let preVC = self.presentingViewController
+//        guard let vc = preVC as? ViewController else {
+//            return
+//        }
+//
+//        // 값을 전달한다
+//        vc.paramEmail = self.email.text
+//        vc.paramUpdate = self.isUpdate.isOn
+//        vc.paramInterval = self.interval.value
+//
+//        // 이전 화면으로 복귀한다
+//        self.presentingViewController?.dismiss(animated: true)
+        
+        
+        // Case 2
+//        // AppDelegate 객체의 인스턴스를 가져온다.
+//        let ad = UIApplication.shared.delegate as? AppDelegate
+//
+//        // 값을 저장한다
+//        ad?.paramEmail = self.email.text
+//        ad?.paramUpdate = self.isUpdate.isOn
+//        ad?.paramInterval = self.interval.value
+//
+//        // 이전 화면으로 복귀한다
+//        self.presentingViewController?.dismiss(animated: true)
+        
+        
+        // Case 3
+        // UserDefault 객체의 인스턴스를 가져온다
+        let ud = UserDefaults.standard
+        
+        //값을 저장한다
+        ud.set(self.email.text, forKey: "email")
+        ud.set(self.isUpdate.isOn, forKey: "isUpdate")
+        ud.set(self.interval.value, forKey: "interval")
+        
+        // 이전화면으로 복귀한다
+        self.presentingViewController?.dismiss(animated: true)
+    }
+    
+    
+}
---
+++ CH5/SubmitValue-Back/SubmitValue-Back/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  SubmitValue-Back
+//
+//  Created by 기태욱 on 2023/03/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // 값을 화면에 표시하기 위한 Outlet 변수들
+    @IBOutlet var resultEmail: UILabel!
+    @IBOutlet var resultUpdte: UILabel!
+    @IBOutlet var resultInterval: UILabel!
+
+    // 값을 직접 전달받을 프로퍼티들
+//    var paramEmail : String?
+//    var paramUpdate : Bool?
+//    var paramInterval : Double?
+    
+    
+    // 화면이 표시될 때마다 실행되는 메서드
+    // - 이미 만들어져 있는 인스턴스임으로 viewDidLoad가 아니라 viewWillAppear
+    override func viewWillAppear(_ animated: Bool) {
+//        if let email = paramEmail {
+//            resultEmail.text = email
+//        }
+//
+//        if let update = paramUpdate {
+//            resultUpdte.text = update==true ? "자동갱신" : "자동갱신안함"
+//        }
+//
+//        if let interval = paramInterval {
+//            resultInterval.text = "\(Int(interval))분마다"
+//        }
+        
+        
+        // Case 2
+//        // AppDelegate 객체의 인스턴스를 가져온다
+//        let ad = UIApplication.shared.delegate as? AppDelegate
+//
+//        if let email = ad?.paramEmail {
+//            resultEmail.text = email
+//        }
+//        if let update = ad?.paramUpdate {
+//            resultUpdte.text = update==true ? "자동갱신" : "자동갱신안함"
+//        }
+//        if let interval = ad?.paramInterval {
+//            resultInterval.text = "\(Int(interval))분마다"
+//        }
+        
+        
+        // Case 3
+        // UserDefaults 객체의 인스턴스를 가져온다
+        let ud = UserDefaults.standard
+        
+        if let email = ud.string(forKey: "email"){
+            resultEmail.text = email
+        }
+        
+        let update = ud.bool(forKey: "isUpdate")
+        resultUpdte.text = (update == true ? "자동갱신" : "자동갱신안함")
+        
+        let interval = ud.double(forKey: "interval")
+        resultInterval.text = "\(Int(interval))분마다"
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
