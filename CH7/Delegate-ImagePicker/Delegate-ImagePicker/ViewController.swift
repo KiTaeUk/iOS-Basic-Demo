1f5732ecbf86798e26c06fdd710c212f2087e8fb
KiTaeUk <ktw1421@naver.com>
2023-05-30 17:48:34 +0900

    [CH7] 델리게이트 패턴

---
+++ CH7/Delegate-ImagePicker/Delegate-ImagePicker/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  Delegate-ImagePicker
+//
+//  Created by 기태욱 on 2023/05/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var imgView: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func pick(_ sender: Any) {
+        // 이미지 피커 컨트롤러 인스턴스 생성
+        let picker = UIImagePickerController()  // 라이브러리 선택
+        picker.sourceType = .photoLibrary       // 이미지 편집 기능
+        picker.allowsEditing = true
+        
+        // +++ 추가된 부분 : 델리게이트 지정
+        picker.delegate = self
+        
+        
+        // 이미지 피커 컨트롤러 실행
+        self.present(picker, animated: false)
+        // info.plist -> Privacy - Photo Library Usage Description or Camera Usage Description 키 : YES 값
+    }
+}
+
+
+// MARK: - 이미지 피커 컨트롤러 델리게이트 메서드
+extension ViewController : UIImagePickerControllerDelegate {
+    
+    // 피커에서 이미지를 선택안하고 취소했을 때 실행되는 메서드
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // 피커 컨트롤러 창 닫기
+        //    = picker.presentingViewController?.dismiss(animated: false)
+        //    = self.dismiss(animated: false)
+        // 내부적으로 알아서 연결시켜줌 -> 이 메서드가 호출되면
+        //  1) 가장 먼저 자신이 치워야 할 뷰 컨트롤러가 있는지 확인
+        //  2) 만약 자신의 위에 다른 화면이 덮고 있다면 그것을 치우지만 아무 화면도 없다면 잘못된 요청으로 간주하고 self.presen.. 객체에 전달
+        picker.dismiss(animated: false) {
+            // 알림창 호출
+            let alert = UIAlertController(title: "", message: "이미지 선택이 취소되었습니다", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "확인", style: .cancel))
+            self.present(alert, animated: false)
+        }
+    }
+    
+    
+    
+    // 피커에서 이미지를 선택했을 때 실행되는 메서드
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        // 이미지 피커 컨트롤러 창 닫기
+        picker.dismiss(animated: false) { () in
+            // 이미지를 이미지 뷰에 표시
+            let img = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+            self.imgView.image = img
+        }
+    }
+    
+}
+
+
+// MARK: - 내비게이션 컨트롤러 델리게이트 메서드
+extension ViewController : UINavigationControllerDelegate {
+    
+}
+
+
---
+++ CH7/Delegate-TextField/Delegate-TextField/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  Delegate-TextField
+//
+//  Created by 기태욱 on 2023/05/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var tf: UITextField!
+    
+
+    
+    override func viewDidLoad() {
+
+        // 텍스트 필드. 속성 설정
+        self.tf.placeholder = "값을 입력하세요"
+        self.tf.keyboardType = UIKeyboardType.alphabet
+        self.tf.keyboardAppearance = UIKeyboardAppearance.dark
+        self.tf.returnKeyType = UIReturnKeyType.join
+        self.tf.enablesReturnKeyAutomatically = true
+        
+        // 스타일 설정
+        self.tf.borderStyle = UITextField.BorderStyle.line
+        self.tf.contentVerticalAlignment = .center
+        self.tf.contentHorizontalAlignment = .center
+        self.tf.layer.borderColor = UIColor.darkGray.cgColor
+        self.tf.layer.borderWidth = 2.0
+        
+        
+        // 텍스트 필드를 최초 응답자로 지정
+        // self.tf.becomeFirstResponder()
+        
+        // 델리게이트 지정
+        self.tf.delegate = self
+        // "텍스트필드에 미리 정해진 이벤트가 발생하면 현재 뷰 컨트롤러에 알려달라는 요청"
+    }
+
+    @IBAction func input(_ sender: Any) {
+        // 텍스트 필드를 최초 응답자 객체로 지정
+        self.tf.becomeFirstResponder()
+
+    }
+    
+    @IBAction func confirm(_ sender: Any) {
+        // 텍스트 필드를 최초 응답자 객체에서 해제
+        self.tf.resignFirstResponder()
+    }
+    
+    
+    
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        print("텍스트 필드의 편집이 시작됩니다")
+        return true
+    }
+
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        print("텍스트 필드의 내용이 삭제됩니다")
+        return true        // false를 리턴하면 삭제되지 않는다
+    }
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        print("텍스트 필드의 내용이 \(string)으로 변경됩니다")
+        // 숫자입력 차단
+        if Int(string) == nil {     // 입력된 값이 숫자가 아니라면 true를 리턴
+            if (textField.text?.count)! + string.count > 10 {
+                return false
+            } else {        // 입력된 값이 숫자라면 false 리턴
+                return true
+            }
+        } else{
+            return false
+        }
+        return true
+    }
+    // 이를 응용하면 특정 문자열을 입력 차단하거나 제어할 수 있다
+    // 델리게이트 메서드는 이처럼
+    // 특정 객체에서 발생하는 이벤트에 맞추어 우리가 원하는 기능을 실행시키고 싶을 때 유용한 해결책으로 사용된다.
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        print("텍스트 필드의 리턴키가 눌러졌습니다")
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        print("텍스트 필드의 편집이 종료됩니다")
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("텍스트 필드의 편집이 종료되었습니다.")
+    }
+    
+}
+
